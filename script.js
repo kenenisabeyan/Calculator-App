000e3f8d3855a4680db4b7d421d86afa8f33fd4d
Kenenisa Beyan <kenenisab05@gmail.com>
2026-02-07 19:36:27 +0200

    add .js

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ function clearAll() {
 function deleteLast() {
   display.value = display.value.slice(0, -1);
 }
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
